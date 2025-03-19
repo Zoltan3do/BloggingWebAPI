bc5a3b4c8ff06db1984ec6b99afe97b2b7493d41
Manuel Barone <120578535+Zoltan3do@users.noreply.github.com>
2025-03-19 23:01:26 +0100

    build

---
+++ src/main/java/com/zoltan/bloggingwebapi/BloggingwebapiApplication.java
@@ -0,0 +1,16 @@
+package com.zoltan.bloggingwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class BloggingwebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggingwebapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zoltan/bloggingwebapi/BloggingwebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoltan.bloggingwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingwebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
